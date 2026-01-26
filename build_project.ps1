835408420325ab2ce9e07fcac249f2aa6b8e198d
Eric Steenwerth <esteenwerth@gmail.com>
2026-01-26 11:23:18 -0500

    Remove obsolete build directories and Jucer build files

--- build_project.ps1
+++
@@ -1,5 +0,0 @@
-$vsDevCmd = "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat"
-$arch = "amd64"
-
-# Run the build
-cmd /c "call `"$vsDevCmd`" -arch=$arch && cmake --build build_nmake_final --config Debug"
